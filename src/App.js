a7fed0194a38ec93b3ec3d753914cf57b593c827
Gunnar Abrahamsson <nvtesp@gmail.com>
2020-07-03 16:04:14 +0200

    Added Provider for user context and cleaned up app

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { AuthProvider } from './auth/userContext'
+import ProtectedRoute from './components/protectedRoute/protectedRoute'
 import LoginPage from './components/loginPage/loginPage';
+import Main from './components/main/main'
 
 import './App.scss';
 
 function App() {
-	const [user, setUser] = useState(null)
-
-	const login = (e) => {
-		console.log('login')
-		setUser(user ? null : true);
-	}
 	return (
-		<div className="App">
-			<LoginPage user={user} login={login} />
-			<button onClick={login}>{user ? 'logout': 'login'}</button>
-		</div>
+		<AuthProvider>
+			<Switch>
+				<Route exact path="/login" component={LoginPage} />
+				<ProtectedRoute path="/" component={Main} />
+			</Switch>
+		</AuthProvider>
 	);
 }
 
